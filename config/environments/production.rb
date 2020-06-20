a29615bbcf2e9d810e7b8460d9b217e23d6fd795
Zippi01 <katyakovtun080619@gmail.com>
2020-06-20 13:34:47 +0300

    heroku fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: 'morning-beyond-33836.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'quiet-peak-11782.herokuapp.com/' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       address: 'smtp.gmail.com',
